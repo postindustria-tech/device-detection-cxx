12881649f4020c21cc92fe1ef5f97f922eb54357
Automation51D <51DCI@51Degrees.com>
2024-08-13 12:04:53 +0000

    Update documentation

---
+++ 4.4/navtreedata.js
@@ -0,0 +1,48 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+    [ "General Docs", "../../documentation/4.4/index.html", null ],
+    [ "51Degrees Device Detection API", "md__home_runner_work_device-detection-cxx_device-detection-cxx_common_device-detection-cxx__r_e_a_d_m_e.html", null ],
+    [ "51Degrees Common Code Library", "md__home_runner_work_device-detection-cxx_device-detection-cxx_common_device-detection-cxx_src_common-cxx__r_e_a_d_m_e.html", null ],
+    [ "C Modules", "modules.html", "modules" ],
+    [ "C++ Namespaces", "namespaces.html", "namespaces" ],
+    [ "Examples", "examples.html", "examples" ]
+];
+
+var NAVTREEHIDEROOT = true;
+
+var NAVTREEHIDESYNC = true;
+
+var NAVTREEINDEX =
+[
+"../../documentation/4.4/index.html",
+"group___fifty_one_degrees_hash_synonyms.html#ga90e4ca7b9a7843dcfd2ab2a2340ca694",
+"group___fifty_one_degrees_synonyms.html#ga35724dea4b14021ba2c322f6ec28b1bc",
+"group___fifty_one_degrees_threading.html#ga73811e87d8e1da7f10bc3943591333ec",
+"structfiftyone_degrees_evidence_prefix_map.html#a024d51159c05241767a02de08aeed1e7"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
