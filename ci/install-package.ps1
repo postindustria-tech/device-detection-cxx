60045323f838ec1f639d1269bf39875dc1823038
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-04-10 22:39:05 +0200

    Update install-package.ps1

--- ci/install-package.ps1
+++ ci/install-package.ps1
@@ -2,11 +2,14 @@ param (
     [string]$RepoName
 )
 
-$WorkDir = [IO.Path]::Combine($pwd, $RepoName)
 $PackagePath = [IO.Path]::Combine($pwd, "package")
+$WorkDir = [IO.Path]::Combine($pwd, $RepoName)
+$BuildDir = [IO.Path]::Combine($WorkDir, "build")
+
 
 # Copy the prebuilt build dir with binaries and artifacts for tests into workdir
 Copy-Item -Path $PackagePath/* -Destination $WorkDir -Recurse
+chmod -R 777 $BuildDir
 
 Write-Output "PackagePath contents:"
 ls $PackagePath
