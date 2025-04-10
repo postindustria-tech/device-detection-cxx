19c2ea65718d832b383f71e519ac920dfdb96f97
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-04-10 21:59:27 +0200

    Update install-package.ps1

--- ci/install-package.ps1
+++ ci/install-package.ps1
@@ -9,13 +9,13 @@ $BinPath = [IO.Path]::Combine($BuildPath, "bin")
 mkdir $BuildPath
 
 # Copy the prebuilt binaries to the build directory
-Copy-Item -Recurse -Path $PackagePath -Destination $BinPath
+#Copy-Item -Recurse -Path $PackagePath -Destination $BinPath
 Copy-Item -Path $PackagePath -Destination $BinPath -Recurse
 
 Write-Output "PackagePath contents:"
 ls $PackagePath
 
-Write-Output "BinPath contents:"
+Write-Output "BuildPath contents:"
 ls $BuildPath
 
 Write-Output "BinPath contents:"
